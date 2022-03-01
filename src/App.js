5d79cb27aceaec44ac60698ced31d2a3a2d07319
Naresh <chandanbatven@gmail.com>
2022-03-02 01:31:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+constructor(props){
+  super(props);
+   this.state={
+     newItem:"",
+     list:[]
+   };
+}
+
+
+
+
+//incorporating local storage 
+componentDidMount() {
+  this.hydrateStateWithLocalStorage();
+
+  // add event listener to save state to localStorage
+  // when user leaves/refreshes the page
+  window.addEventListener(
+    "beforeunload",
+    this.saveStateToLocalStorage.bind(this)
   );
 }
 
+componentWillUnmount() {
+  window.removeEventListener(
+    "beforeunload",
+    this.saveStateToLocalStorage.bind(this)
+  );
+
+  // saves if component has a chance to unmount
+  this.saveStateToLocalStorage();
+}
+
+hydrateStateWithLocalStorage() {
+  // for all items in state
+  for (let key in this.state) {
+    // if the key exists in localStorage
+    if (localStorage.hasOwnProperty(key)) {
+      // get the key's value from localStorage
+      let value = localStorage.getItem(key);
+
+      // parse the localStorage string and setState
+      try {
+        value = JSON.parse(value);
+        this.setState({ [key]: value });
+      } catch (e) {
+        // handle empty string
+        this.setState({ [key]: value });
+      }
+    }
+  }
+}
+
+saveStateToLocalStorage() {
+  // for every item in React state
+  for (let key in this.state) {
+    // save to localStorage
+    localStorage.setItem(key, JSON.stringify(this.state[key]));
+  }
+}
+
+
+
+
+updateInput(key, value){
+  this.setState({
+    [key]:value
+  });
+}
+
+addItem() {
+   const newItem={
+     id: 1+Math.random(),
+     value: this.state.newItem.slice()
+   };
+
+   const list = [...this.state.list];
+
+   list.push(newItem);
+
+   this.setState({
+     list,
+     newItem:""
+   });
+
+}
+
+deleteItem(id){
+  const list = [...this.state.list];
+
+  const updatedList = list.filter(item => item.id != id);
+
+  this.setState({list: updatedList});
+}
+
+  render() {
+    return(
+   <div className="App">
+      
+        <div className="head"> Your Tasks </div>
+        <br/>
+        <br/>
+        <input
+         type="text"
+         placeholder="Enter your task name..."
+         value={this.state.newItem}
+         onChange={e => this.updateInput("newItem", e.target.value)}
+        />
+            &nbsp;
+        <button
+         onClick={() => this.addItem()}
+        >
+          Add
+        </button>
+
+        <br/>
+
+        <ul>
+          {this.state.list.map(item => {
+
+          return(
+            <li key={item.id}>
+            {item.value}
+            &nbsp;
+            <button 
+            onClick={() => this.deleteItem(item.id)}
+            >
+             X
+            </button>
+          </li>
+          )
+           
+          })}
+        </ul>
+
+     
+   </div>
+    )
+  };
+}
+
+
 export default App;
+
